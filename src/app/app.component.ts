12633940d4722e8d8a0e1058690016cbcae90d6d
Stylianos Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-03-11 17:08:06 +0200

    Added service pass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  // Prices
+  payPerBumpInSol = 0.00019;
+  tokenPassInSol = 0.048;
+  servicePassInSol = 0.38;
+
   title = 'pump-fun-bot-website';
 }
