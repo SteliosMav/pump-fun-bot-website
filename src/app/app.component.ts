a3805730a3c6ca4403c1bdda75488e627dd15570
Stylianos Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-03-20 22:56:51 +0200

    changed prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   // Prices
-  payPerBumpInSol = 0.00019;
-  tokenPassInSol = 0.048;
+  payPerBumpInSol = 0.00008;
+  tokenPassInSol = 0.038;
   servicePassInSol = 0.38;
 
   title = 'pump-fun-bot-website';
